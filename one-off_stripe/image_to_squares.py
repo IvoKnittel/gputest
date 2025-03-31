5f0131e7aac4d8cc2f12100e8113a8d03818a5a8
Ivo Knittel <Ivo.Knittel@web.de>
2025-03-31 22:23:40 +0200

    generate 2x2 squares

--- one-off_stripe/image_to_squares.py
+++ one-off_stripe/image_to_squares.py
@@ -1,11 +1,29 @@
 import numpy as np
-import numpy.typing as npt
 from item import Item
-import math
 
+def crop(n):
+    num_tiles = int(np.floor(n-2)/4)
+    return 4*num_tiles + 2 , num_tiles
 
-def load_image(image):
-    for i in range(0,image.shape[0]):
-        for j in range(0, image.shape[1]):
-            pixel = image[i,j]
+def image_squares(image):
+    height_cropped, num_tiles_vertical = crop(image.shape[0])
+    width_cropped, num_tiles_horizontal = crop(image.shape[1])
 
+    image_cropped = image((height_cropped,width_cropped))
+    image_1x2_items = np.empty((height_cropped, width_cropped), dtype=Item)
+    for i in range(0, 2*num_tiles_vertical + 1):
+        for j in range(0, width_cropped):
+            image_1x2_items[2*i,j] = Item(Item(image_cropped[2*i,j]), Item(image_cropped[2*i+1,j]))
+
+    for i in range(0, 2*num_tiles_vertical):
+        for j in range(0, width_cropped):
+            image_1x2_items[2*i+1,j] = Item(Item(image_cropped[2*i+1,j]), Item(image_cropped[2*i+2,j]))
+
+    image_2x2_items = np.empty((height_cropped, width_cropped), dtype=Item)
+    for i in range(0, height_cropped):
+        for j in range(0, 2*num_tiles_horizontal+1):
+            image_2x2_items[i,2*j] = Item(image_1x2_items[2*i,j]), Item(image_1x2_items[2*i+1,j])
+
+    for i in range(0, height_cropped):
+        for j in range(0, 2*num_tiles_horizontal):
+            image_2x2_items[i,2*j+1] = Item(image_1x2_items[2*i+1,j]), Item(image_1x2_items[2*i+2,j])
--- one-off_stripe/test_stripes.py
+++ one-off_stripe/test_stripes.py
@@ -1,4 +1,4 @@
-from pickletools import uint8
+from image_to_squares import image_squares
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +7,9 @@ def test_load():
     image_path = 'image_monochrom.bmp'
     image = Image.open(image_path)
     image = image.convert('1')
-    np_array = np.array(image).astype(np.uint8)
-    plt.imshow(np_array, cmap='gray')
+    image_array = np.array(image).astype(np.uint8)
+    image2x2 = image_squares(image_array)
+
+    plt.imshow(image_array, cmap='gray')
     plt.axis('on')
     plt.show()
