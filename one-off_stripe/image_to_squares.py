e13d1ef0918ec39f35da2f992e9650e90a8e40a0
Ivo Knittel <Ivo.Knittel@web.de>
2025-04-04 20:22:33 +0200

    final ranking of squares

--- one-off_stripe/image_to_squares.py
+++ one-off_stripe/image_to_squares.py
@@ -27,3 +27,33 @@ def image_squares(image):
     for i in range(0, height_cropped):
         for j in range(0, 2*num_tiles_horizontal):
             image_2x2_items[i,2*j+1] = Item(image_1x2_items[2*i+1,j]), Item(image_1x2_items[2*i+2,j])
+
+def image_squares_ranked0(image_squares):
+    squares_ranked0 = np.zeros((2*image_squares.shape[0],2*image_squares.shape[1]), dtype=int)
+    for i in range(1, image_squares.shape[0]):
+        for j in range(1, image_squares.shape[1]):
+
+            qvec=np.zeros(4, dtype=float)
+            qvec[0] = image_squares[i - 1, j - 1].quality
+            qvec[1] = image_squares[i - 1, j].quality
+            qvec[2] = image_squares[i, j - 1].quality
+            qvec[3] = image_squares[i, j].quality
+            indices = np.argsort(qvec)
+            q=(2*i,2*j)
+            squares_ranked0[q[0], q[1]] = indices[0]
+            squares_ranked0[q[0], q[1] + 1] = indices[1]
+            squares_ranked0[q[0] + 1, q[1]] = indices[2]
+            squares_ranked0[q[0] + 1, q[1] + 1] = indices[3]
+
+    return squares_ranked0
+
+def image_squares_ranked(r):
+    s = np.zeros(r.shape, dtype=float)
+    for i in range(2, r.shape[0]-2):
+        for j in range(2, r.shape[1]-2):
+            s[2 * i, 2 * j] = np.mean([r[2 * (i - 1) + 1, 2 * (j - 1) + 1], r[2 * (i - 1) + 1, 2 * j], r[2 * i, 2 * (j - 1)], r[2 * i, 2 * j + 1]])
+            s[2 * i, 2 * j + 1] = np.mean([r[2 * (i - 1) + 1, 2 * j + 1], r[2 * (i - 1) + 1, 2 * (j + 1)], r[2 * i, 2 * j],r[2 * i, 2 * (j + 1) + 1]])
+            s[2 * i + 1, 2 * j] = np.mean([r[2 * i + 1, 2 * (j - 1) + 1], r[2 * i + 1, 2 * j], r[2 * (i + 1), 2 * (j - 1)],r[2 * (i + 1), 2 * j + 1]])
+            s[2 * i + 1, 2 * j + 1] = np.mean([r[2 * i + 1, 2 * j], r[2 * i + 1, 2 * (j + 1)], r[2 * (i + 1), 2 * j], [2 * (i + 1), 2 * (j + 1)]])
+    return s
+#  def image_rank_quA(image_squares):
